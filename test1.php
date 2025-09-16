62cca5ec9ff9aeefa04e41cfc1943a1a6eafd2c0
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-09-16 20:37:36 +0800

    test

--- test1.php
+++ test1.php
@@ -1,17 +1,17 @@
 <?php
 
+use Library\Defer\Defer;
 use Library\Defer\Parallel;
+use Library\Defer\Process;
 
 require __DIR__ . '/vendor/autoload.php';
 
 
 
 $start = microtime(true);
-Parallel::all([
-    fn() =>  sleep(1),
-    fn() => sleep(1),
-    fn() => sleep(1),
-    fn() => sleep(1),
-]);
+$taskId = Defer::background(function() {
+   sleep(4);
+   file_put_contents('test.txt', 'hello world');
+});
 $endtime = microtime(true);
 echo "Total Time: " . $endtime - $start . " seconds";
